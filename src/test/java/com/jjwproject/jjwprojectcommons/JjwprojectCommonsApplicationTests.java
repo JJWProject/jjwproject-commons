bef16a8ed48518af85eed698636f2d8da4ed02ba
MightyCupcakes <13433484+MightyCupcakes@users.noreply.github.com>
2022-05-04 13:54:37 +0800

    Initial Commit

---
+++ src/test/java/com/jjwproject/jjwprojectcommons/JjwprojectCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjwproject.jjwprojectcommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjwprojectCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
